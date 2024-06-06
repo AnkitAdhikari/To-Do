6ed8f51863f0a0a9a173d4b1d487f6984d2abc87
AnkitAdhikari <ankitadhikari31512@gmail.com>
2024-06-06 23:34:37 +0545

    add task organization in multiple projects and view all projects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 class List {
     constructor() {
         this.list = [];
@@ -20,12 +21,13 @@ class Task {
     note;
     checkList = new List();
 
-    constructor(name, dueDate = new Date().toISOString(), priority = 1, note) {
+    constructor(name, dueDate = format(new Date(), "yyyy-MM-dd"), priority = 1, note, id) {
         this.name = name;
         this.dueDate = dueDate;
         this.priority = priority;
         this.note = note;
         this.isComplete = false;
+        this.id = id
     }
 
     update(newName, newStatus, newDate, newNote, newPriority) {
@@ -38,7 +40,7 @@ class Task {
     }
 
     updateStatus(newStatus) {
-        this.isComplete = newStatus || this.isComplete;
+        this.isComplete = newStatus
     }
 
     rename(newName) {
@@ -66,7 +68,9 @@ class Tasks {
     }
     delete(id) {
         if (id >= 0 && id < this.tasks.length) {
-            this.tasks.splice(id, 1);
+            const deleated = this.tasks.splice(id, 1);
+            this.tasks.forEach((task, i, _) => task.id = i);
+            return deleated[0];
         }
     }
 }
@@ -90,17 +94,43 @@ class Project {
 new class Projects {
     projects = [new Project("Default")];
     constructor() {
-        PubSub.publish('All_Projects', this.projects);
+        PubSub.publish('Render_Projects', this.projects);
         PubSub.subscribe('Add_Project', (msg, data) => {
             this.add();
-            PubSub.publish('Project_Added', this.projects);
+            PubSub.publish('Render_Projects', this.projects);
         })
         PubSub.subscribe('Rename_Project', (msg, data) => {
             this.renameProject(+data.id, data.newName);
         });
         PubSub.subscribe('Delete_Project', (msg, data) => {
             this.delete(+data.id);
-            PubSub.publish('Project_Added', this.projects);
+            PubSub.publish('Render_Projects', this.projects);
+        })
+        PubSub.subscribe('Open_Project', (msg, data) => {
+            PubSub.publish('Render_Tasks', { tasks: this.getAllTasks(data.id).tasks, projectId: data.id });
+        })
+        PubSub.subscribe('Add_Task', (msg, data) => {
+            this.addTask(data.id);
+            PubSub.publish('Render_Tasks', { tasks: this.getAllTasks(data.id).tasks, projectId: data.id })
+            PubSub.publish('Display_Modal', { task: this.getAllTasks(data.id).tasks.at(-1), projects: this.projects });
+        })
+        PubSub.subscribe('Delete_Task', (msg, data) => {
+            this.deleteTask(data.projectId, data.taskId);
+            PubSub.publish('Render_Tasks', { tasks: this.getAllTasks(data.projectId).tasks, projectId: data.projectId });
+            PubSub.publish('Close_Modal', {});
+        })
+        PubSub.subscribe('Update_Task', (msg, data) => {
+            this.updateTask(data.projectId, data.taskId, data.name, data.status, data.date, data.description, data.taskPriority);
+            PubSub.publish('Render_Tasks', { tasks: this.getAllTasks(data.projectId).tasks, projectId: data.projectId })
+        })
+        PubSub.subscribe('Open_Task', (msg, data) => {
+            PubSub.publish('Display_Modal', { task: this.getAllTasks(data.projectId).tasks.at(data.taskId), projects: this.projects });
+        })
+        PubSub.subscribe('Change_Task_Project', (msg, { newId, oldId, taskId }) => {
+            this.moveTask({ newId, oldId }, taskId);
+        });
+        PubSub.subscribe('All_Tasks', (msg, data) => {
+            this.allTasks();
         })
     }
     defaultName() {
@@ -122,7 +152,13 @@ new class Projects {
             this.projects.splice(id, 1);
         }
     }
-    addTask(name = '', note = '', priority = 1, date, id = 0) {
+
+    getAllTasks(projectId) {
+        const project = this.#getProject(projectId);
+        return project.getTasks();
+    }
+
+    addTask(id = 0, name = '', note = '', priority = 1, date) {
         if (id >= 0 && id < this.projects.length) {
             if (id === 0) {
                 this.projects[0].todo.add(name, date, priority, note)
@@ -136,9 +172,16 @@ new class Projects {
         task.update(newName, newStatus, newDate, newNote, newPriority)
     }
     deleteTask(projectid, taskid) {
-        console.log(this.#getProject(projectid));
-        this.#getProject(projectid).getTasks(taskid).delete(taskid);
+        return this.#getProject(projectid).getTasks(taskid).delete(taskid);
     }
+
+    moveTask(projectid, taskid) {
+        const task = this.deleteTask(projectid.oldId, taskid);
+        const project = this.projects[projectid.newId]
+        project.todo.tasks.push(task);
+        PubSub.publish('Open_Project', { id: projectid.oldId });
+    }
+
     addList(projectid, taskid, name) {
         this.#getProject(projectid).getTask(taskid).checkList.add(name);
     }
@@ -147,5 +190,12 @@ new class Projects {
     }
     deleteList(projectid, taskid, listid) {
         this.#getProject(projectid).getTask(taskid).checkList.delete(listid);
+    }
+
+    allTasks() {
+        this.projects.forEach((project, i, _) => {
+            PubSub.publish("Render_All_Tasks", { tasks: project.todo.tasks, projectId: i });
+        })
+        PubSub.publish("All_Tasks_Rendered", {});
     }
 }
--- src/indexGUI.js
+++ src/indexGUI.js
@@ -1,36 +1,115 @@
 import './style.css';
 
 import PubSub from 'pubsub-js'
+import { format } from "date-fns";
 
 
 new class GUI {
     constructor() {
+        this.currentActiveProjectId = 0;
+        this.currentActiveTask = null;
+
+        this.modal = document.createElement('dialog');
         this.cacheStaticDOM();
-        PubSub.subscribe('All_Projects', (msg, data) => {
-            console.log(data)
+        this.bindStaticEvent();
+        PubSub.subscribe('Render_Projects', (msg, data) => {
             this.renderProjects(data);
         })
-        PubSub.subscribe('Project_Added', (msg, data) => {
-            this.renderProjects(data);
+        PubSub.subscribe('Render_Tasks', (msg, tasks) => {
+            this.renderTasks(tasks);
+        })
+        PubSub.subscribe('Display_Modal', (msg, data) => {
+            this.projects = data.projects;
+            this.showModal(data.task, data.projects);
+        })
+        PubSub.subscribe('Close_Modal', (msg, data) => this.modal.close());
+        PubSub.subscribe("Render_All_Tasks", (msg, { tasks, projectId }) => {
+            this.renderTasks({ tasks, projectId, multi: true });
+        })
+        PubSub.subscribe('All_Tasks_Rendered', (msg, data) => {
+            this.appendAddTaskBtn();
+            this.cacheDynamicTaskDOM();
+            this.bindTaskEvent();
         });
-
     }
 
     cacheStaticDOM() {
         this.projectListContainer = document.getElementById('project-list-container')
+        this.content = document.querySelector('.content');
+        this.allTasks = document.querySelector('.all-tasks');
+        this.todayTasks = document.querySelector('.today-tasks');
+        this.weekTasks = document.querySelector('.week-tasks');
+        this.highAlertTasks = document.querySelector('.high-alert-tasks');
     }
 
-    cacheDynamicDOM() {
+    cacheDynamicProjectDOM() {
         this.addProjectBtn = document.querySelector('.add-project-btn');
         this.projectNameField = document.querySelectorAll('.project-name')
         this.projectDeleteBtn = document.querySelectorAll('.project-del-btn')
+        this.projects = document.querySelectorAll('.project')
     }
 
-    bindEvent() {
+    cacheDynamicTaskDOM() {
+        this.addTaskBtn = document.querySelector('.add-task-btn');
+        this.tasks = document.querySelectorAll('.task');
+        this.taskExpandBtns = document.querySelectorAll('.task-expand-btn');
+    }
+    cacheModalDOM() {
+        this.closeModalBtn = document.querySelector('.close-modal-btn')
+        this.deleteTaskBtn = document.querySelector('.task-del-btn');
+        this.taskisCompleted = document.querySelector('.task-isCompleted');
+        this.taskName = document.querySelector('.task-name');
+        this.taskDescription = document.querySelector('.task-description')
+        this.taskDate = document.querySelector('.task-Due-date');
+        this.priorityBtns = document.querySelectorAll('.task-priority');
+        this.priorityDivs = document.querySelector('.priorityDivs');
+        this.projectOpt = document.querySelector('.project-opt')
+    }
+
+    bindStaticEvent() {
+        this.allTasks.addEventListener('click', () => {
+            this.content.innerHTML = '';
+            PubSub.publish('All_Tasks', {});
+        })
+    }
+
+    bindProjectEvent() {
         this.addProjectBtn.addEventListener('click', this.addProject.bind(this));
         this.projectNameField.forEach(project => project.addEventListener('input', this.renameProject.bind(this)));
         this.projectDeleteBtn.forEach(project => project.addEventListener('click', this.deleteProject.bind(this)));
+        this.projects.forEach(project => project.addEventListener('click', this.openProject.bind(this)));
+    }
 
+    bindTaskEvent() {
+        this.addTaskBtn.addEventListener('click', this.addTask.bind(this));
+        this.taskExpandBtns.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                const taskId = e.target.dataset.taskid;
+                const projectId = e.target.dataset.projectid;
+                this.currentActiveProjectId = projectId;
+                PubSub.publish('Open_Task', { taskId, projectId })
+            })
+        })
+    }
+
+    bindModalEvent() {
+        this.closeModalBtn.addEventListener('click', () => {
+            const newId = this.projectOpt.value
+            const oldId = this.currentActiveProjectId;
+            PubSub.publish('Change_Task_Project', { newId, oldId, taskId: this.currentActiveTask });
+            this.modal.close();
+        })
+        this.deleteTaskBtn.addEventListener('click', this.deleteTask.bind(this));
+        [this.taskisCompleted, this.taskName, this.taskDescription, this.taskDate].forEach(el => el.addEventListener('input', (e) => {
+            PubSub.publish('Update_Task', { name: this.taskName.value, status: this.taskisCompleted.checked, description: this.taskDescription.value, projectId: this.currentActiveProjectId, taskId: +this.taskName.dataset.taskid, date: this.taskDate.value });
+        }))
+        this.priorityBtns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                PubSub.publish('Update_Task', { name: this.taskName.value, status: this.taskisCompleted.checked, description: this.taskDescription.value, projectId: this.currentActiveProjectId, taskId: +this.taskName.dataset.taskid, date: this.taskDate.value, taskPriority: btn.textContent })
+                this.priorityDivs.querySelector('.active').classList.remove('active');
+                btn.classList.add('active');
+            })
+        })
     }
 
     addProject() {
@@ -38,7 +117,6 @@ new class GUI {
     }
 
     renameProject(e) {
-        console.log(e.target);
         const id = e.target.dataset.projectid;
         const newName = e.target.value;
         PubSub.publish('Rename_Project', { newName, id });
@@ -49,21 +127,156 @@ new class GUI {
         PubSub.publish('Delete_Project', { id });
     }
 
+    openProject(e) {
+        if (e.target === e.currentTarget) {
+            const id = e.currentTarget.dataset.projectid;
+            this.updateCurrentActiveProject(id);
+            PubSub.publish('Open_Project', { id })
+        }
+    }
+
+    updateCurrentActiveProject(id) {
+        this.currentActiveProjectId = id;
+    }
+
+    addTask() {
+        PubSub.publish('Add_Task', { id: this.currentActiveProjectId });
+    }
+
+    showModal(task, projects) {
+        this.modal.innerHTML = '';
+        this.modal.insertAdjacentHTML('afterbegin', ` <div class="modal">
+        <div class="modal-head">
+          <ion-icon
+            data-projectID="${this.currentActiveProjectId}"
+            data-taskID="${task.id}"
+            name="trash"
+            class="task-del-btn"
+          ></ion-icon>
+          <ion-icon class="close-modal-btn" name="close-circle-outline"></ion-icon>
+        </div>
+        <div class="modal-body">
+          <div class="modal-left">
+            <div class="tasks-content">
+              <div>
+                <input ${task.isComplete ? "checked" : ''} class="task-isCompleted" data-projectID="${this.currentActiveProjectId}"
+                data-taskID="${task.id}" type="checkbox" name="" id="" /><input
+                data-projectID="${this.currentActiveProjectId}"
+            data-taskID="${task.id}"
+                class="task-name"
+                  type="text"
+                 value="${task.name}"
+                  placeholder="Task Name"
+                />
+              </div>
+              <div>
+                <ion-icon name="newspaper-outline"></ion-icon>
+                <textarea data-projectID="${this.currentActiveProjectId}"
+                data-taskID="${task.id}"
+                  style="resize: none"
+                  class="task-description"
+                  placeholder="Your description"
+                >${task.note}</textarea>
+              </div>
+            </div>
+            <div class="lists-content">
+              <div class="heading">
+                <ion-icon name="list-outline"></ion-icon> Checklist
+              </div>
+              <ul class="lists"></ul>
+            </div>
+          </div>
+          <div class="modal-right">
+            <div>
+              <p>Project</p>
+              <select class="project-opt">
+              ${projects.map((project, i, _) => {
+            return `<option data-taskid="${task.id}" ${i == this.currentActiveProjectId ? 'selected' : ''} value="${i}">${project.name}</option>`
+        }).join(' ')}
+              </select>
+            </div>
+            <div>
+              <p>Due Date</p>
+              <input
+                class="task-Due-date"
+                type="date"
+                name=""
+                id=""
+                value="${task.dueDate}"
+              />
+            </div>
+            <div>
+              <p>Priority</p>
+              <div class="priorityDivs">
+                <div data-projectID="${this.currentActiveProjectId}"
+                data-taskID="${task.id}" class="task-priority ${1 == task.priority ? "active" : ''}" style="background-color: teal">1</div>
+                <div data-projectID="${this.currentActiveProjectId}"
+            data-taskID="${task.id}" class="task-priority ${2 == task.priority ? "active" : ''}" style="background-color: green">2</div>
+                <div data-projectID="${this.currentActiveProjectId}"
+            data-taskID="${task.id}" class="task-priority ${3 == task.priority ? "active" : ''}" style="background-color: goldenrod">3</div>
+                <div data-projectID="${this.currentActiveProjectId}"
+            data-taskID="${task.id}" class="task-priority ${4 == task.priority ? "active" : ''}" style="background-color: burlywood">4</div>
+                <div data-projectID="${this.currentActiveProjectId}"
+            data-taskID="${task.id}" class="task-priority ${5 == task.priority ? "active" : ''}" style="background-color: brown">5</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`);
+        this.content.insertAdjacentElement('afterend', this.modal);
+        this.modal.showModal();
+        this.cacheModalDOM();
+        this.bindModalEvent();
+        this.currentActiveTask = task.id;
+    }
+
+    deleteTask(e) {
+        const projectId = +e.target.dataset.projectid;
+        const taskId = +e.target.dataset.taskid;
+        PubSub.publish('Delete_Task', { projectId, taskId })
+    }
 
     renderProjects(projects) {
         this.projectListContainer.innerHTML = '';
         projects.forEach((project, id, _) => {
-            this.projectListContainer.insertAdjacentHTML('beforeend', `<li class="nav-list">
+            this.projectListContainer.insertAdjacentHTML('beforeend', `<li class="nav-list project" data-projectID="${id}">
             <ion-icon name="grid" class="nav-icon"></ion-icon
             ><input class="project-name" ${id === 0 ? 'readonly' : ''} data-projectID="${id}" type="text" value="${project.name}" />
             <ion-icon data-projectID="${id}" name="trash" class="project-del-btn"></ion-icon>
           </li>`)
         });
         this.appendAddProjectBtn();
-        this.cacheDynamicDOM();
-        this.bindEvent();
+        this.cacheDynamicProjectDOM();
+        this.bindProjectEvent();
     }
     appendAddProjectBtn() {
         this.projectListContainer.insertAdjacentHTML('beforeend', `<li class="add-project-btn">Add +</li>`)
     }
+
+    renderTasks({ tasks, projectId, multi = false }) {
+        if (!multi) {
+            this.content.innerHTML = '';
+        }
+        tasks.forEach((task, id, _) => {
+            this.content.insertAdjacentHTML('beforeend', `          <div
+            class="task"
+            data-task-id="${id}"
+          >
+            <input ${task.isComplete ? "checked" : ''} type="checkbox" name="is-completed" class="task__is-done" />
+            <h3 class="task-title">${task.name}</h3>
+            <ion-icon data-taskid=${id} data-projectid=${projectId} name="expand-outline" class="task-expand-btn"></ion-icon>
+            <h3 class="task-date">${task.dueDate}</h3>
+          </div>`);
+        })
+        if (!multi) {
+            this.appendAddTaskBtn();
+            this.cacheDynamicTaskDOM();
+            this.bindTaskEvent();
+        }
+    }
+
+    appendAddTaskBtn() {
+        this.content.insertAdjacentHTML('beforeend', `<div class="add-task-btn" data-projecid="${this.currentActiveProjectId}" >Add +</div>`)
+    }
+
 }
